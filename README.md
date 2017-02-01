6f55208eb10c6f9e2239f4921bf57e1dce1e843c
CJ Burkey <cjburkey01@gmail.com>
2017-02-01 16:03:13 -0500

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BurkAR
+
 Simply AutoRanking
+
+Latest version: [1.0.0](http://github.com/cjburkey01/BurkAR/releases/tag/1.0.0) for [Spigot 1.11](https://www.spigotmc.org/).
+
+View on [SpigotMC](http://bit.ly/2kth834)
---
+++ src/com/cjburkey/plugin/ar/BurkAR.java
@@ -0,0 +1,17 @@
+package com.cjburkey.plugin.ar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BurkAR extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
